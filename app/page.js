1d65113438d024c2453e4ad632e920824caf5156
RichardVergara09 <richarddeanvergara@gmail.com>
2024-07-31 13:51:05 -0600

    Commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+      <main>
+        <h1 className="text-3xl" >Welcome to the NBA Stats!</h1>
+        <h2>Assignment 1: Personal Website</h2>
+      </main>
+  );
+}
